1bebe3bb99c6226f9973a9ce2880fd19927046b7
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-01-28 16:05:29 -0500

    added nav bar with links

--- blog-app-thunder/src/App.js
+++ blog-app-thunder/src/App.js
@@ -1,13 +1,14 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './screens/Home/Home';
 import UserList from './screens/UserList';
 import UserDetail from './screens/UserList';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
-      <Link to="/users">Users</Link>
+      <Nav />
 
       <Routes>
         <Route path="/" element={<Home />} />
