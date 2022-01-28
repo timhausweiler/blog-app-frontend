eeef25648de0734a9d17c7f6e0204cda9aac18e4
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-01-28 17:49:03 -0500

    deleted unused import from home component

--- blog-app-thunder/src/screens/Home/Home.jsx
+++ blog-app-thunder/src/screens/Home/Home.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Home.css';
 
@@ -14,8 +14,6 @@ export default function Home() {
   const [input, setInput] = useState(login_input);
 
   const navigate = useNavigate();
-
-  useEffect(() => {}, []);
 
   const handleLoginChange = (e) => {
     const { id, value } = e.target;
