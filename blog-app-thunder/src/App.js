6411eb0352b945f0c8be0f53df3f5cf9009a4131
Kevin Cleary <kjc2274@yahoo.com>
2022-01-28 17:53:02 -0500

    some packagejson stuff

--- blog-app-thunder/src/App.js
+++ blog-app-thunder/src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 import Home from './screens/Home/Home';
 import UserList from './screens/UserList';
 import UserDetail from './screens/UserDetail';
-<<<<<<< HEAD
 import Signup from './screens/Signup/Signup';
-=======
-import Signup from './screens/Signup';
->>>>>>> 41743f7c117409847d0a6caa9276a0de9b35f810
 import Nav from './components/Nav/Nav';
 
 function App() {
