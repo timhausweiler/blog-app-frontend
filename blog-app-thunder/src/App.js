5ce2866532c3ad4acef57438287162c54691a4cc
timhausweiler <haustimp@gmail.com>
2022-01-28 16:09:06 -0500

    list of users working

--- blog-app-thunder/src/App.js
+++ blog-app-thunder/src/App.js
@@ -8,17 +8,8 @@ import Nav from './components/Nav/Nav';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
-       <Routes>
-=======
-      <Link to="/users">Users</Link>
-=======
       <Nav />
->>>>>>> 41da971c843971578c2e6b341396702d8cf3386b
-
       <Routes>
->>>>>>> 3218d93ca0cc42e05c11c65e348a81b64a22c521
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<UserList />} />
         <Route path="/user/:id" element={<UserDetail />} />
