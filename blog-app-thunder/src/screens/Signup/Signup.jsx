b3160e9dec837d268689ae1e31ec4e5c854e9c76
Kevin Cleary <kjc2274@yahoo.com>
2022-01-30 11:33:19 -0500

    getting ready to import changes

--- blog-app-thunder/src/screens/Signup/Signup.jsx
+++ blog-app-thunder/src/screens/Signup/Signup.jsx
@@ -28,9 +28,7 @@ export default function Signup() {
   const handleSubmit = async (event)=>{
     event.preventDefault();
     const fields = input;
-    console.log(fields);
-    await axios.post("http://localhost:3000/api/signup", {fields});
-    console.log(input.userName);
+    await axios.post("http://localhost:3000/api/signup", fields);
     setInput(default_input);
     navigate("/");
   }
