3866700c8207e40981d907e3f5a8e9f56c36ece2
timhausweiler <haustimp@gmail.com>
2022-01-30 13:31:03 -0500

    checking out

--- src/db/blog.connection.js
+++ src/db/blog.connection.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 
 const db = mongoose.connect;
 const MONGODB_URI =
-	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/users";
+	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/blogposts";
 
-export const initMongoServer = () => {
+export const initMongoBlogServer = () => {
 	try {
 		db(MONGODB_URI).catch(error => {
 			throw error;
