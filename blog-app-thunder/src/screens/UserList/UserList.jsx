448b5b22b43a1d32336bfc46b74d650f36d83bbf
timhausweiler <haustimp@gmail.com>
2022-01-31 16:06:48 -0500

    deploying now

--- blog-app-thunder/src/screens/UserList/UserList.jsx
+++ blog-app-thunder/src/screens/UserList/UserList.jsx
@@ -1,16 +1,18 @@
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Nav from '../../components/Nav/Nav';
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Nav from "../../components/Nav/Nav";
 
 export default function UserList() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const res = await axios.get('https://kkt-backend.herokuapp.com/api/users');
+      const res = await axios.get(
+        "https://kkt-backend.herokuapp.com/api/users"
+      );
       console.log(res);
-      console.log(res.data.data)
+      console.log(res.data.data);
       setUsers(res.data.data);
     };
     fetchUsers();
@@ -19,14 +21,14 @@ export default function UserList() {
   return (
     <div>
       <Nav />
-      <h2>Overview of all users</h2>
+      <h2>Overview of all users!</h2>
       {users?.map((user, index) => {
         return (
           <Link to={`/user/${user._id}`} key={index}>
             <div>
               <h3>{user.userName}</h3>
               <p>{user.email}</p>
-              <img src={user.avatar} alt = "avatar"/>
+              <img src={user.avatar} alt="avatar" />
             </div>
           </Link>
         );
