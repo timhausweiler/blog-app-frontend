7115a3c8e7f9fb0d355635c0a956fd8db5a7f9fa
timhausweiler <haustimp@gmail.com>
2022-01-31 16:16:02 -0500

    whatever

--- src/controllers/defaultController.js
+++
@@ -1,8 +0,0 @@
-import errorHandler from "../utilities/error.js";
-
-const defaultController = async (req, res, next) => {
-	res.json(errorHandler(false, "Home", "Welcome to my user API!"));
-	res.status(200);
-};
-
-export default defaultController;
